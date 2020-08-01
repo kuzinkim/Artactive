107851921cf6cfdb5839fe705ad2a11519c29e1d
Kuzin Kim <vadikpetrov53@gmail.com>
2020-08-01 19:54:57 +0300

    delete sourcemap

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,13 +1,1 @@
-var debugTimes = false;
-
-/**
- * Global Variables
- */
-
-/**
- * Document Ready
- */
-$(document).ready(function () {
-    
-});
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJjb21tb24uanMiXSwic291cmNlc0NvbnRlbnQiOlsidmFyIGRlYnVnVGltZXMgPSBmYWxzZTtcclxuXHJcbi8qKlxyXG4gKiBHbG9iYWwgVmFyaWFibGVzXHJcbiAqL1xyXG5cclxuLyoqXHJcbiAqIERvY3VtZW50IFJlYWR5XHJcbiAqL1xyXG4kKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbiAoKSB7XHJcbiAgICBcclxufSk7Il0sImZpbGUiOiJjb21tb24uanMifQ==
+var debugTimes=!1;$(document).ready(function(){});
